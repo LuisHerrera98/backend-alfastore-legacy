749ccb5c3bad111c5df165cb86c406341d7846ef
luisherrera98 <luchoherrerafernandez@gmail.com>
2023-09-12 12:47:08 -0300

    update index

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const app = express();
 app.use(morgan("dev"));
 const corsOptions = {
   origin: "*",
-  credentials: true, //access-control-allow-credentials:true
+  credentials: false, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions)); // Use this after the variable declaration
